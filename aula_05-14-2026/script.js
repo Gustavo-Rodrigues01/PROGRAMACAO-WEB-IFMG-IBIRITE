01876ce8d13495a757a98ae1d6b7c7e0969c6219
Gustavo Dos Reis Rodrigues <0116998@ifmg.edu.br>
2026-05-14 12:33:16 -0300

    Adicionando aula do dia 14/05/2026

---
+++ aula_05-14-2026/script.js
@@ -0,0 +1,13 @@
+let num = 1;
+console.log("numeros de 1 a 10")
+while(num <= 10){
+    console.log(num);
+    num++;
+};
+
+num--
+console.log("numeros de 10 a 1")
+while(num >= 1){
+    console.log(num);
+    num--;
+};
